30261afeb2b33240dd204e0c0c40f925c49a2286
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2025-04-04 13:26:24 -0300

    cadastro colaborador OK, relatórios e editar parciais

--- epi2025/epi/epi_app/urls.py
+++ epi2025/epi/epi_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
     path('cadastro/', views.cadastro_colaborador, name='cadastro_colaborador'),
     path('relatorios/', views.relatorios, name='relatorios'),
     path('editar/', views.editar_colaborador, name='editar_colaborador'),
--- epi2025/epi/epi_app/views.py
+++ epi2025/epi/epi_app/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
+from .models import Colaboradores
 
 # Create your views here.
 def home(request):
-    return render(request, 'epi_app/pages/home.html')
+    return render(request, 'epi_app/pages/home.html') 
 def cadastro_colaborador(request):
-    return render(request, 'epi_app/pages/cadastro.html')
+    if request.method == 'GET':
+        return render(request, 'epi_app/pages/cadastro.html')
+    nome = request.POST.get('nome')
+    cargo = request.POST.get('cargo')    
+    setor = request.POST.get('setor')
+    colaborador = Colaboradores.objects.create(nome=nome, cargo=cargo, setor=setor)
+    return render(request, 'epi_app/pages/cadastro.html', context={'colaborador': colaborador})
 
 def relatorios(request):
-    return render(request, 'epi_app/pages/relatorios.html')
+    lista_colaboradores = Colaboradores.objects.all()
+    return render(request, 'epi_app/pages/relatorios.html', context={'colaboradores': lista_colaboradores})
 
 def editar_colaborador(request):
     return render(request, 'epi_app/pages/editar.html')
