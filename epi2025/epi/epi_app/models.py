a895f212c8955a49ae366132b43bd09b751cc3f9
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2025-04-04 10:50:19 -0300

    ccriação classe colabs, makemigrations e migrate executados

--- epi2025/epi/epi_app/models.py
+++ epi2025/epi/epi_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+# cpf = models.CharField(max_length=11)
+# email = models.EmailField()
+# telefone = models.CharField(max_length=20)
 # Create your models here.
+class Colaboradores(models.Model):
+    nome = models.CharField(max_length=100)
+    cargo = models.CharField(max_length=100)
+    setor = models.CharField(max_length=100)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    alterado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Colaborador({self.nome}, {self.cargo}, {self.setor})"
