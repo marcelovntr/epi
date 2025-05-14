af1d24854fe28ee381be99d755daf5ad02612f13
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2025-05-14 18:39:55 -0300

    versão com alterações da segunda aula de testes, exceto divisão da views em arquivos separados

--- epi2025/epi/epi_app/urls.py
+++ epi2025/epi/epi_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    
     path('cadastro/', views.cadastro_colaborador, name='cadastro_colaborador'),
     path('relatorios/', views.relatorios, name='relatorios'),
     path('editar/<int:id>', views.editar_colaborador, name='editar_colaborador'),
